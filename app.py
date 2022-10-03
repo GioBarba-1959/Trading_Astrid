5bcd51b5b099b28d85b8b5dc40f807b9de8bb333
GioBarba-1959 <111189146+GioBarba-1959@users.noreply.github.com>
2022-10-03 10:28:31 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import json, os
-from xml.etree.ElementTree import tostring
 import ccxt
 from time import sleep
 from datetime import datetime
@@ -10,11 +9,10 @@ from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
 
-#app.secret_key = "manbearpig_MUDMAN888"
 apikey=os.environ.get('API_KEY')
 apisecret=os.environ.get('API_SECRET')
-qty=0.001#os.environ.get('QTY_ORDER')
-tp=1.5#os.environ.get('TP')
+qty=os.environ.get('QTY')
+tp=os.environ.get('TP')
 
 client = Client(apikey, apisecret)
 exchange = ccxt.binance({
@@ -28,40 +26,45 @@ exchange.load_markets()
 
 def orderBuy(quantity, symbol, order_price):
     try:
-        #order = client.order_limit_buy(
-        #    symbol=symbol,
-        #    quantity=quantity,
-        #    price=order_price)
+        
         order=client.order_market_buy(
             symbol=symbol,
             quantity=quantity
         )
+        #order = client.create_order(symbol=symbol, side=side, type='BUY', quantity=quantity, timeInForce='GTC', price=order_price)
         print(order)
         response=True
+        #print(f"{passphrase} {symbol} Sending order {order_type} - {side} {quantity} {symbol} at {order_price}.")        
+        #print(f" {passphrase} {symbol} Not Executed order {order_type} - {side} {quantity} {symbol} at {order_price}.")
     except Exception as e:
         print("An exception occured - {}".format(e))
         return False
     return response
 
-def orderSell(quantity, symbol, order_price):
+def orderOCO(stopLoss, quantity, symbol, order_price,timeInForce='GTC'):
     try:
-        #order = client.order_limit_sell(
-        #    symbol=symbol,
-        #    quantity=quantity,
-        #    price=order_price)
-        order=client.order_market_sell(
-            symbol=symbol,
-            quantity=quantity
-        )
-        print(order)
-        response=True
+        market = exchange.market(symbol)
+        response = exchange.private_post_order_oco({
+        'symbol': market['id'],
+        'side': 'SELL',  # SELL, BUY
+        'quantity': exchange.amount_to_precision(symbol, quantity),
+        'price': exchange.price_to_precision(symbol, order_price),
+        'stopPrice': exchange.price_to_precision(symbol, stopLoss),
+        'stopLimitPrice': exchange.price_to_precision(symbol, stopLoss),  # If provided, stopLimitTimeInForce is required
+        'stopLimitTimeInForce': timeInForce,  # GTC, FOK, IOC
+        })
+        print(response)
+       
     except Exception as e:
         print("An exception occured - {}".format(e))
         return False
-    return response  
+    
+    return response
+
+
 @app.route('/')
 def welcome():
-    return "<h1>Questo è il trading bot di ASTRID</h1>"
+    return "<h1>Questo è il mio trading ver. 2.0 bot BTC/USDT</h1>"
 
 @app.route("/hello")
 def index():
@@ -86,19 +89,43 @@ def webhook():
     side = data['strategy']['order_action'].upper()
     quantity = float(qty) #0.001  data['strategy']['order_contracts']
     ticker = data['ticker'].upper()
-    ticker='BTCUSDT'
+    ticker='BTC/USDT'
+    #--------- funziona solo per ordini BUY -----------------
+    if side != 'BUY':
+        return {
+            "code": "Annullato",
+            "message": "Si eseguono solo ordini di acquisto."
+        }
+    #--------- controllo che non vi siano ordini aperti -----
+    orders=client.get_open_orders(symbol='BTCUSDT')
+    if orders:
+        return {
+            "code": "Annullato",
+            "message": "Ordine non evaso per la presenza di un ordine precedente aperto."
+        }
+    
+    #--------- arrotondamenti e calcolo prezzo vendita e stopLoss
     order_price = round(data['strategy']['order_price'],2)
-    
-    
+    stopLoss=round(data['strategy']['stopLoss'],2)
+    diff_P_SL=order_price-stopLoss
+    diff_P_TP=diff_P_SL*float(tp)
+    orderOCO_price=round(order_price+diff_P_TP,2)
 
-    if side == 'BUY':
-          order_response = orderBuy(quantity,ticker, order_price)
-       
-    else:
-          order_response = orderSell(quantity,ticker, order_price)
-   
+    #order_response = order(side, quantity, ticker, order_price, data['passphrase'])
+    order_response = orderBuy(quantity,'BTCUSDT', order_price)
 
     if order_response:
+        #sleep(60)
+				
+        orderOCO_response= orderOCO(stopLoss, quantity, ticker, orderOCO_price)
+        if orderOCO_response:
+            return {
+                "code": "success","message": "Order + OrderOCO executed."}
+        else:
+            print("OrderOCO Failed.")
+            return {
+            "code": "error","message": "OrderOCO Failed."
+        }
         return {
             "code": "success",
             "message": "Order executed."
